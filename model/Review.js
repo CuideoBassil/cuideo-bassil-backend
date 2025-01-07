3608b9517c5fca648449fb89d32b83f5fdf6b96e
RamiMetry <99014901+RamiMetry@users.noreply.github.com>
2025-01-07 21:41:29 +0200

    fix

--- model/Review.js
+++ model/Review.js
@@ -3,7 +3,7 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const reviewSchema = new mongoose.Schema(
   {
-    userId: { type: ObjectId, ref: "User", required: true },
+    userId: { type: ObjectId, ref: "User", required: fasle },
     productId: {
       type: ObjectId,
       ref: "Products",
