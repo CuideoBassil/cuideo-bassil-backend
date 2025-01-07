eef648bba9047e32aacb2916066351abb08ebc4d
RamiMetry <99014901+RamiMetry@users.noreply.github.com>
2025-01-07 21:38:18 +0200

    review fixes

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -5,29 +5,29 @@ const Review = require("../model/Review");
 const User = require("../model/User");
 
 // add a review
-exports.addReview = async (req, res,next) => {
+exports.addReview = async (req, res, next) => {
   const { userId, productId, rating, comment } = req.body;
   try {
     // Check if the user has already left a review for this product
-    const existingReview = await Review.findOne({
-      user: userId,
-      product: productId,
-    });
+    // const existingReview = await Review.findOne({
+    //   user: userId,
+    //   product: productId,
+    // });
 
-    if (existingReview) {
-      return res
-        .status(400)
-        .json({ message: "You have already left a review for this product." });
-    }
-    const checkPurchase = await Order.findOne({
-      user: new mongoose.Types.ObjectId(userId),
-      "cart._id": { $in: [productId] },
-    });
-    if (!checkPurchase) {
-      return res
-        .status(400)
-        .json({ message: "Without purchase you can not give here review!" });
-    }
+    // if (existingReview) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "You have already left a review for this product." });
+    // }
+    // const checkPurchase = await Order.findOne({
+    //   user: new mongoose.Types.ObjectId(userId),
+    //   "cart._id": { $in: [productId] },
+    // });
+    // if (!checkPurchase) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Without purchase you can not give here review!" });
+    // }
 
     // Create the new review
     const review = await Review.create(req.body);
@@ -39,28 +39,28 @@ exports.addReview = async (req, res,next) => {
     await product.save();
 
     // Add the review to the user's reviews array
-    const user = await User.findById(userId);
-    user.reviews.push(review._id);
-    await user.save();
+    // const user = await User.findById(userId);
+    // user.reviews.push(review._id);
+    // await user.save();
 
     return res.status(201).json({ message: "Review added successfully." });
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
 };
 
 // delete a review
-exports.deleteReviews = async (req, res,next) => {
+exports.deleteReviews = async (req, res, next) => {
   try {
     const productId = req.params.id;
-    const result = await Review.deleteMany({productId: productId });
+    const result = await Review.deleteMany({ productId: productId });
     if (result.deletedCount === 0) {
-      return res.status(404).json({ error: 'Product reviews not found' });
+      return res.status(404).json({ error: "Product reviews not found" });
     }
-    res.json({ message: 'All reviews deleted for the product' });
+    res.json({ message: "All reviews deleted for the product" });
   } catch (error) {
     console.log(error);
-    next(error)
+    next(error);
   }
 };
